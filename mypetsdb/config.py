e1181f76fbd6f68a81a5cf22e731fb3bf2befc31
Mitchell Broome <mbroome@centos7.attlocal.net>
2019-03-24 17:38:57 -0400

    The order of loading configs is a bit better now

--- mypetsdb/config.py
+++ mypetsdb/config.py
@@ -5,8 +5,8 @@ import json
 dirPath = os.path.dirname(os.path.realpath(__file__))
 
 class ConfigLoader():
-   def __init__(self, configFile=''):
-      self.configFile = configFile
+   def __init__(self, configFiles=[]):
+      self.configFileList = configFiles
       self.config = {}
 
       # see if we can find the file in the dir tree
@@ -15,34 +15,25 @@ class ConfigLoader():
          while p:
             c = p + '/settings.json'
             if os.path.exists(c):
-               self.configFile = c
-               break
+               self.configFileList.insert(0, c)
             p = p[:p.rfind('/')]
          
       # is there one in /etc/config
-      if not self.configFile:
-         if os.path.exists('/etc/config/settings.json'):
-            self.configFile = '/etc/config/settings.json'
+      self.configFileList.append('/etc/config/settings.json')
 
       # what about one set in the environment
       if 'APP_CONFIG' in os.environ:
-         self.configFile = os.environ['APP_CONFIG']
+         self.configFileList.append(os.environ['APP_CONFIG'])
 
-      # parse up the config file
-      content = open(self.configFile, 'r').read()
-      data = json.loads(content)
-
-      # of there is a 'default' section, populate it
-      if 'default' in data:
-         self.config = data['default'].copy()
-
+      for f in self.configFileList:
+         self.mergeConfig(f)
 
       self.config['TOP_LEVEL_DIR'] = os.path.dirname(os.path.realpath(__file__)) + '/../'
 
       # pick the section of the config based on the APP_ENV
       if 'APP_ENV' in os.environ:
-         if os.environ['APP_ENV'] in data:
-            self.config.update(data[ os.environ['APP_ENV']])
+         for f in self.configFileList:
+            self.mergeConfig(f, os.environ['APP_ENV'])
 
       # override configs with environment variables, if set
       for key in os.environ.keys():
@@ -51,7 +42,25 @@ class ConfigLoader():
             self.config[k] = os.environ[key]
 
    def __getattr__(self, key):
-      return self.config[key]
+      if key in self.config:
+         return(self.config[key])
+      else:
+         return(False)
+
+   def mergeConfig(self, configFile, section='default'):
+      # parse up the config file
+      try:
+         content = open(configFile, 'r').read()
+         data = json.loads(content)
+      except Exception, e:
+         #print e
+         pass
+
+      try:
+         self.config.update(data[section])
+      except Exception, e:
+         #print e
+         pass
 
 settings = ConfigLoader()
 
